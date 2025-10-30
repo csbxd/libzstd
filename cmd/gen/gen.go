64bb1288030907c77b125ff9f02729a03b5845f1
不插电 <r27153733@gmail.com>
2025-10-31 04:35:57 +0800

    feat: zstd windows gen.

--- cmd/gen/gen.go
+++ cmd/gen/gen.go
@@ -5,7 +5,6 @@ import (
 	"os/exec"
 	"path/filepath"
 	"runtime"
-	"strings"
 
 	ccgo "modernc.org/ccgo/v4/lib"
 )
@@ -29,6 +28,14 @@ func init() {
 		panic(err)
 	}
 	srcDir = dir
+}
+
+func env(name, deflt string) (r string) {
+	r = deflt
+	if s := os.Getenv(name); s != "" {
+		r = s
+	}
+	return r
 }
 
 func PrepareDirectory() (tempDir string) {
@@ -57,52 +64,51 @@ func ApplyPatch(tempDir string) {
 }
 
 func CombineZSTDSources(tempDir string) {
-	run(filepath.Join(tempDir, "zstd/build/single_file_libs"),
+	singleDir := filepath.Join(tempDir, "zstd/build/single_file_libs")
+	run(singleDir,
 		"python3", "combine.py",
 		"-r", "../../lib", "-x", "legacy/zstd_legacy.h", "-o", "../../../zstd.c", "zstd-in.c")
+
 }
 
 func CompileToGo(zstdCPath string) {
-	targets := strings.Split(os.Getenv("ZSTD_GEN_TARGETS"), ";")
-	ccs := strings.Split(os.Getenv("ZSTD_GEN_CCS"), ";")
-	if targets[0] == "" {
-		targets[0] = runtime.GOOS + "_" + runtime.GOARCH
-	}
+	goarch := env("TARGET_GOARCH", env("GOARCH", runtime.GOARCH))
+	goos := env("TARGET_GOOS", env("GOOS", runtime.GOOS))
 
-	for i := range targets {
-		t := strings.Split(targets[i], "_")
-		tOS := t[0]
-		tArch := t[1]
-
-		err := os.Setenv("CC", ccs[i])
+	switch goos {
+	case "darwin":
+		err := ccgo.NewTask(goos, goarch, []string{
+			"ccgo", "--package-name", "libzstd", "-std=c17",
+			// libc not implement qsort_r.
+			"-ignore-link-errors",
+			// __darwin_arm_neon_state64 __darwin_arm_neon_state use __int128, ignore.
+			"-ignore-unsupported-alignment",
+			zstdCPath, "-o", filepath.Join(srcDir, "zstd_"+goos+"_"+goarch+".go"),
+		}, os.Stdout, os.Stderr, nil).Main()
 		if err != nil {
 			panic(err)
 		}
-
-		switch tOS {
-		case "darwin":
-			err = ccgo.NewTask(tOS, tArch, []string{
-				"ccgo", "--package-name", "libzstd", "-std=c17",
-				// libc not implement qsort_r.
-				"-ignore-link-errors",
-				// __darwin_arm_neon_state64 __darwin_arm_neon_state use __int128, ignore.
-				"-ignore-unsupported-alignment",
-				zstdCPath, "-o", filepath.Join(srcDir, "zstd_"+targets[i]+".go"),
-			}, os.Stdout, os.Stderr, nil).Main()
-			if err != nil {
-				panic(err)
-			}
-		case "linux":
-			err = ccgo.NewTask(tOS, tArch, []string{
-				"ccgo", "--package-name", "libzstd", "-std=c17",
-				zstdCPath, "-o", filepath.Join(srcDir, "zstd_"+targets[i]+".go"),
-			}, os.Stdout, os.Stderr, nil).Main()
-			if err != nil {
-				panic(err)
-			}
+	case "linux":
+		err := ccgo.NewTask(goos, goarch, []string{
+			"ccgo", "--package-name", "libzstd", "-std=c17",
+			zstdCPath, "-o", filepath.Join(srcDir, "zstd_"+goos+"_"+goarch+".go"),
+		}, os.Stdout, os.Stderr, nil).Main()
+		if err != nil {
+			panic(err)
+		}
+	case "windows":
+		err := ccgo.NewTask(goos, goarch, []string{
+			"ccgo", "--package-name", "libzstd", "-std=c17",
+			// libc not implement qsort_s clock.
+			"-ignore-link-errors",
+			// ignore some builtin func
+			"-D_IMMINTRIN_H_INCLUDED", "-D_FMA4INTRIN_H_INCLUDED", "-D_XOPMMINTRIN_H_INCLUDED",
+			zstdCPath, "-o", filepath.Join(srcDir, "zstd_"+goos+"_"+goarch+".go"),
+		}, os.Stdout, os.Stderr, nil).Main()
+		if err != nil {
+			panic(err)
 		}
 	}
-
 }
 
 func main() {
